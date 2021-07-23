78b597824e6febe01b5dcd44e4f398d95a9295d8
rururu-orca <87410304+rururu-orca@users.noreply.github.com>
2021-07-23 13:52:04 +0900

    ・Viewプロジェクト、Paket追加

---
+++ src/RandomSceneDrawing/App.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+module RandomSceneDrawing.App
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+open FSharp.Control.Reactive
+
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+    let application =
+        Application.LoadComponent <| Uri("App.xaml", UriKind.Relative)
+        :?> Application
+
+    application.Run()
