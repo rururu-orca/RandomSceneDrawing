ef3d461d229461acca88e47caf8e2f5a376fa2fe
rururu-orca <87410304+rururu-orca@users.noreply.github.com>
2021-07-24 20:44:43 +0900

    動作確認:LibVLCSharpによる動画再生

--- src/RandomSceneDrawing.Core/PlayerLib.fs
+++ src/RandomSceneDrawing.Core/PlayerLib.fs
@@ -3,5 +3,9 @@ module RandomSceneDrawing.PlayerLib
 open System
 open LibVLCSharp.Shared
 
+Core.Initialize()
+let libvlc = new LibVLC("--verbose=2")
+let mediaPlayer = new MediaPlayer(libvlc)
+
 let getMediaFromlocal (source: string) libvlc =
-    new Media(libvlc, (Uri source))
+   new Media(libvlc, (Uri source))
