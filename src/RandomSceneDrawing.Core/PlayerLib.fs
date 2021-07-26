e09d573cf393915f79f32e3bd5a2e5459c247f05
rururu-orca <87410304+rururu-orca@users.noreply.github.com>
2021-07-26 19:05:43 +0900

    Cleaning Code

--- src/RandomSceneDrawing.Core/PlayerLib.fs
+++ src/RandomSceneDrawing.Core/PlayerLib.fs
@@ -2,5 +2,52 @@ module RandomSceneDrawing.PlayerLib
 
 open System
 open LibVLCSharp.Shared
+open FsToolkit.ErrorHandling
+open Types
+open LibVLCSharp.Shared
 
-let getMediaFromUri source libvlc = new Media(libvlc, (Uri source))
+do Core.Initialize()
+
+let libVLC =
+#if DEBUG
+    new LibVLC("--verbose=2")
+#else
+    new LibVLC(false)
+#endif
+
+let getMediaFromUri source = new Media(libVLC, (Uri source))
+
+let player = new MediaPlayer(libVLC)
+
+let play source =
+    async{
+        match player.State with
+        | VLCState.NothingSpecial
+        | VLCState.Stopped
+        | VLCState.Ended
+        | VLCState.Error ->
+            use media = getMediaFromUri source
+            player.Play media |> ignore
+            
+            let! e = media.DurationChanged |> Async.AwaitEvent
+            return PlaySuccess {Duration = float e.Duration |> TimeSpan.FromMilliseconds}
+            
+        | VLCState.Paused ->
+            player.Pause() |> ignore
+            return PlaySuccess {Duration = float player.Media.Duration |> TimeSpan.FromMilliseconds}
+        | VLCState.Opening
+        | VLCState.Buffering
+        | VLCState.Playing
+        | _ -> return PlayFailed player.State
+    }
+
+let pause () = async{
+    player.Pause()
+    return PauseSuccess
+}
+let stop () = async {
+    player.Stop()
+    player.Media.DurationChanged |> Async.AwaitEvent |> ignore
+    return StopSuccess
+}
+
