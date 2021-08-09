d56ab2101587b420799327cc875f13af205ae735
rururu-orca <87410304+rururu-orca@users.noreply.github.com>
2021-08-09 19:18:44 +0900

    WIP:Model構築中

--- src/RandomSceneDrawing.Core/DrawingSetvice.fs
+++ src/RandomSceneDrawing.Core/DrawingSetvice.fs
@@ -1,6 +1,41 @@
 module RandomSceneDrawing.DrawingSetvice
 
 open System
+open System.Threading
+open FSharp.Control
+open Elmish
+open System.Windows
+
+module CountDownTimer =
+    type Model =
+        { TimerOn: bool
+          Count: TimeSpan
+          Step: TimeSpan }
+
+    type Msg =
+        | TimedTick
+        | TimerToggled of bool
+
+    type CmdMsg = | TimerTick
+
+    let init () =
+        { TimerOn = false
+          Count = TimeSpan()
+          Step = TimeSpan(0, 0, 1) },
+        Cmd.Empty // An empty list means no action
+
+    let update msg model =
+        match msg with
+        | TimerToggled on -> { model with TimerOn = on }, [ if on then yield TimerTick ]
+        | TimedTick ->
+            match model with
+            | { TimerOn = false } -> model, []
+            | { Count = ct; Step = stp } when ct - stp > TimeSpan.Zero -> { model with Count = ct - stp }, [ TimerTick ]
+            | _ ->
+                { model with
+                      TimerOn = false
+                      Count = TimeSpan.Zero },
+                []
 
 [<Measure>]
 type sec
@@ -8,23 +43,52 @@ type sec
 [<Measure>]
 type msec
 
+
 module TimeSpan =
     let ofSec (sec: float<sec>) = TimeSpan.FromSeconds(float sec)
     let ofMsec (sec: float<msec>) = TimeSpan.FromMilliseconds(float sec)
-    let addSec (sec: float<sec>) (ts: TimeSpan) =
-        ts + TimeSpan.FromSeconds(float sec)
+
+    let addSec (sec: float<sec>) (ts: TimeSpan) = ts + TimeSpan.FromSeconds(float sec)
+
     let addMsec (sec: float<msec>) (ts: TimeSpan) =
         ts + TimeSpan.FromMilliseconds(float sec)
+
+let sec s = (<|) TimeSpan.ofSec s
+
+type Async with
+    static member Sleep(sec: float<sec>) = TimeSpan.ofSec sec |> Async.Sleep
 
 type Model =
     { Frames: int
       Duration: TimeSpan
-      Interval: int }
+      Interval: int
+      Token: CancellationToken }
 
-type Msg = TickSec of float<sec>
+type CmdMessage =
+    | Request
+    | Success
+    | Cancel
+    | Failed of exn
+
+type Msg =
+    | TickSec of float<sec>
+    | StartCountDown of CmdMessage
+
+
+let startCountDown (tick: float<sec>) =
+    async {
+        do! Async.Sleep tick
+        printfn "Starting sleep workflow at %O" DateTime.Now.TimeOfDay
+        do! Async.Sleep tick
+        printfn "Finished sleep workflow at %O" DateTime.Now.TimeOfDay
+        return Success
+    }
 
 let update msg m =
     match msg with
     | TickSec sec ->
         { m with
               Duration = m.Duration |> TimeSpan.addSec -sec }
+    | StartCountDown Request ->
+        Cmd.OfAsync.either startCountDown 1.<sec> id Failed
+        m
