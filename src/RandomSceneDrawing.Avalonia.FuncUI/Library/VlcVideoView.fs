2d4a0364f5c1ddeff95601739a485a0f5ef01226
rururu-orca <87410304+rururu-orca@users.noreply.github.com>
2021-09-10 12:30:46 +0900

    VideoView実装

---
+++ src/RandomSceneDrawing.Avalonia.FuncUI/Library/VlcVideoView.fs
@@ -0,0 +1,284 @@
+namespace LibVLCSharp.Avalonia.FuncUI
+
+open Avalonia
+open Avalonia.Controls
+open Avalonia.Controls.Templates
+open Avalonia.Controls.Presenters
+open Avalonia.Controls.Primitives
+open Avalonia.Media
+open Avalonia.Layout
+open Avalonia.VisualTree
+open Avalonia.Platform
+open Avalonia.Threading
+open Avalonia.Styling
+open System
+open System
+open System.Collections.Generic
+open FSharpPlus
+open FSharp.Control
+open FSharp.Control.Reactive
+
+open RandomSceneDrawing.AvaloniaExtensions
+open LibVLCSharp.Shared
+
+module Util =
+    let bindProperty<'T when 'T :> AvaloniaProperty>
+        disposables
+        (property: 'T)
+        (source: AvaloniaObject)
+        (target: AvaloniaObject)
+        =
+        target.Bind(property, source.GetObservable property)
+        |> Disposable.disposeWith disposables
+
+    let inline addClassHandler< ^T, ^U when ^T :> AvaloniaObject>
+        action
+        (observable: IObservable<AvaloniaPropertyChangedEventArgs< ^U >>)
+        =
+        observable
+        |> Observable.subscribe
+            (fun e ->
+                match e.Sender with
+                | :? ^T as target -> action target e
+                | _ -> ())
+
+    let findNameScope<'T when 'T: not struct> name (namescope: INameScope) =
+        try
+            Some(namescope.Get<'T> name)
+        with
+        | :? KeyNotFoundException -> None
+
+open Util
+
+module FloatingContent =
+
+
+    let getPoint left top =
+        match left, top with
+        | Some x, Some y -> Point(x (), y ())
+        | Some x, None -> Point(x (), 0.0)
+        | None, Some y -> Point(0.0, y ())
+        | None, None -> Point(0.0, 0.0)
+
+    let getLeft (owner: ILayoutable) (target: ILayoutable) () =
+        match target.HorizontalAlignment with
+        | HorizontalAlignment.Right -> owner.Bounds.Width - target.Bounds.Width
+        | HorizontalAlignment.Center -> (owner.Bounds.Width - target.Bounds.Width) / 2.0
+        | _ -> 0.0
+
+    let getTop (owner: ILayoutable) (target: ILayoutable) () =
+        match target.VerticalAlignment with
+        | VerticalAlignment.Bottom -> owner.Bounds.Height - target.Bounds.Height
+        | VerticalAlignment.Center -> (owner.Bounds.Height - target.Bounds.Height) / 2.0
+        | _ -> 0.0
+
+    let fitWindowPosition (floating: Window) (owner: ContentControl) =
+        floating.GetVisualDescendants()
+        |> Seq.tryPick
+            (function
+            | :? VisualLayerManager as m -> Some m
+            | _ -> None)
+        |> Option.map
+            (fun manager ->
+                let getLeft' = Some(getLeft owner manager)
+                let getTop' = Some(getTop owner manager)
+
+                let newValues =
+                    match manager.HorizontalAlignment, manager.VerticalAlignment with
+                    | (HorizontalAlignment.Stretch, VerticalAlignment.Stretch) ->
+                        SizeToContent.Manual, owner.Bounds.Width, owner.Bounds.Height, getPoint None None
+                    | (HorizontalAlignment.Stretch, _) ->
+                        SizeToContent.Width, owner.Bounds.Width, Double.NaN, getPoint None getLeft'
+                    | (_, VerticalAlignment.Stretch) ->
+                        SizeToContent.Height, Double.NaN, owner.Bounds.Height, getPoint getTop' None
+                    | (_, _) -> SizeToContent.Manual, Double.NaN, Double.NaN, getPoint getLeft' getTop'
+
+                manager, newValues)
+        |> Option.iter
+            (fun (manager, (newSizeToContent, newWidth, newHeight, newPoint)) ->
+
+                manager.MaxWidth <- owner.Bounds.Width
+                manager.MaxHeight <- owner.Bounds.Height
+
+                floating.SizeToContent <- newSizeToContent
+                floating.Width <- newWidth
+                floating.Height <- newHeight
+
+                match owner.PointToScreen newPoint with
+                | newPosition when newPosition <> floating.Position -> floating.Position <- newPosition
+                | _ -> ())
+
+    let showAtMe (control: ContentControl) =
+        let disposables = Disposable.Composite
+
+        let floating =
+            Window(
+                SystemDecorations = SystemDecorations.None,
+                TransparencyLevelHint = WindowTransparencyLevel.Transparent,
+                Background = Brushes.Transparent,
+                TransparencyBackgroundFallback = Brushes.Black,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ShowInTaskbar = false
+            )
+
+#if DEBUG
+        floating.AttachDevTools()
+#endif
+
+        let bindToControl (property: 'T) =
+            bindProperty<'T> disposables property control
+
+        let subscribeForUpdatelayout (observable: IObservable<'T>) =
+            Observable.skip 1 observable
+            |> Observable.subscribe (fun _ -> fitWindowPosition floating control)
+            |> Disposable.disposeWith disposables
+
+        let root =
+            (control :> IVisual).VisualRoot :?> Window
+
+        floating
+        |> bindToControl ContentControl.ContentProperty
+
+        control.GetObservable ContentControl.ContentProperty
+        |> subscribeForUpdatelayout
+
+        control.GetObservable ContentControl.BoundsProperty
+        |> subscribeForUpdatelayout
+
+        root.PositionChanged |> subscribeForUpdatelayout
+
+        floating.Show root
+        fitWindowPosition floating control
+
+        { new IDisposable with
+            member x.Dispose() = floating.Close() }
+        |> Disposable.disposeWith disposables
+
+        disposables
+
+type private VlcNativePresenter() =
+    inherit NativeControlHost()
+
+    let mutable platformHandle = Option<IPlatformHandle>.None
+
+    override x.CreateNativeControlCore (parent) =
+        platformHandle <-
+            base.CreateNativeControlCore parent
+            |> Option.ofObj
+
+        Option.toObj platformHandle
+
+    override x.DestroyNativeControlCore (control) =
+        platformHandle <- None
+        base.DestroyNativeControlCore control
+
+    member x.AttachHandle (mediaPlayer : MediaPlayer) =
+        match Environment.OSVersion.Platform, platformHandle with
+        | PlatformID.Win32NT, Some handle -> mediaPlayer.Hwnd <- handle.Handle
+        | PlatformID.MacOSX, Some handle -> mediaPlayer.XWindow <- uint handle.Handle
+        | PlatformID.Unix, Some handle -> mediaPlayer.NsObject <- handle.Handle
+        | _ -> ()
+
+type VideoView() as x =
+    inherit ContentControl()
+    let floatingDisposables = Disposable.Composite
+    let mediaDisposables = Disposable.Composite
+    let mediaPlayerDisposables = Disposable.Composite
+
+    let mutable templateApplied = false
+    let mutable nativePresenter = Option<VlcNativePresenter>.None
+    let mutable nativePresenterPanel = Option<Panel>.None
+    let mutable mediaPlayer = Option<MediaPlayer>.None
+
+    let iterFloating action =
+        fun _ -> nativePresenter |> Option.iter action
+        |> Dispatcher.UIThread.Post
+
+    let subscribeMedia (x: VideoView) (media: Media) =
+        mediaDisposables.Clear()
+
+        media.StateChanged
+        |> Observable.subscribe
+            (fun e ->
+                match e.State with
+                | VLCState.Buffering -> ()
+                | VLCState.Ended -> ()
+                | VLCState.Error -> ()
+                | VLCState.NothingSpecial -> ()
+                | VLCState.Opening -> ()
+                | VLCState.Paused -> ()
+                | VLCState.Playing -> iterFloating (fun f -> f.IsVisible <- true)
+                | VLCState.Stopped -> iterFloating (fun f -> f.IsVisible <- false))
+        |> Disposable.disposeWith mediaDisposables
+
+    let subscribeMediaPlayer (x: VideoView) (mp: MediaPlayer) =
+        mediaPlayerDisposables.Clear()
+
+        mp.MediaChanged
+        |> Observable.subscribe (fun e -> subscribeMedia x e.Media)
+        |> Disposable.disposeWith mediaPlayerDisposables
+
+    do x.Styles.Load "avares://RandomSceneDrawing.Avalonia.FuncUI/Library/VlcVideoViewStyles.xaml"
+
+    interface IVideoView with
+        member x.MediaPlayer
+            with get () = Option.toObj mediaPlayer
+            and set (value) =
+                if x.SetAndRaise(VideoView.MediaPlayerProperty, (Option.toObj >> ref) mediaPlayer, value) then
+                    mediaPlayer <- Option.ofObj value
+                    subscribeMediaPlayer x value
+                    x.InitMediaPlayer()
+
+
+    static member MediaPlayerProperty =
+        AvaloniaProperty.RegisterDirect(
+            nameof MediaPlayer,
+            (fun (o: VideoView) -> (o :> IVideoView).MediaPlayer),
+            (fun (o: VideoView) v -> (o :> IVideoView).MediaPlayer <- v)
+        )
+
+    static member VideoPanel() =
+        [ VideoView.MediaPlayerProperty.Changed
+          |> addClassHandler<VideoView, MediaPlayer> (fun s e -> s.InitMediaPlayer()) ]
+        |> Disposables.compose
+
+    override x.OnApplyTemplate e =
+        base.OnApplyTemplate e
+        nativePresenter <- findNameScope<VlcNativePresenter> "nativePresenter" e.NameScope
+        nativePresenterPanel <- findNameScope<Panel> "nativePresenterPanel" e.NameScope
+
+        templateApplied <- true
+
+        x.InitMediaPlayer()
+
+    member private x.InitMediaPlayer() =
+        match mediaPlayer, nativePresenter with
+        | Some player, Some presenter ->
+
+            presenter.AttachHandle player
+            presenter.IsVisible <- false
+        | _ -> ()
+
+    override x.Render context =
+        if floatingDisposables.Count = 0 then
+            FloatingContent.showAtMe x
+            |> Disposable.disposeWith floatingDisposables
+
+        base.Render context
+
+    override x.OnDetachedFromVisualTree e =
+
+        floatingDisposables.Dispose()
+        mediaDisposables.Dispose()
+        mediaPlayerDisposables.Dispose()
+
+        base.OnDetachedFromVisualTree e
+
+module VideoView =
+    open Avalonia.FuncUI.Builder
+    open Avalonia.FuncUI.Types
+    let create (attrs: IAttr<VideoView> list) : IView<VideoView> = ViewBuilder.Create<VideoView>(attrs)
+
+    let mediaPlayer<'t when 't :> VideoView> (player: MediaPlayer) : IAttr<'t> =
+        AttrBuilder<'t>
+            .CreateProperty<MediaPlayer>(VideoView.MediaPlayerProperty, player, ValueNone)
--- src/RandomSceneDrawing.Avalonia.FuncUI/Program.fs
+++ src/RandomSceneDrawing.Avalonia.FuncUI/Program.fs
@@ -1,14 +1,135 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+namespace RandomSceneDrawing
 
 open System
 open FSharpPlus
+open LibVLCSharp.Shared
+open LibVLCSharp.Avalonia.FuncUI
 
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open Avalonia.FuncUI.DSL
 
-[<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+module Counter =
+    open Avalonia.Controls
+    open Avalonia.Layout
+
+    Core.Initialize()
+
+    let libVLC = new LibVLC()
+    let player = new MediaPlayer(libVLC)
+
+    type State = { count: int }
+    let init = { count = 0 }
+
+    type Msg =
+        | Increment
+        | Decrement
+        | SetCount of int
+        | Reset
+
+    let update (msg: Msg) (state: State) : State =
+        match msg with
+        | Increment ->
+            { state with count = state.count + 1 }
+        | Decrement -> { state with count = state.count - 1 }
+        | SetCount count -> { state with count = count }
+        | Reset -> init
+
+    let view (state: State) (dispatch) =
+        DockPanel.create [
+            DockPanel.children [
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch Reset)
+                    Button.content "reset"
+                    Button.horizontalAlignment HorizontalAlignment.Stretch
+                ]
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch Decrement)
+                    Button.content "-"
+                    Button.horizontalAlignment HorizontalAlignment.Stretch
+                ]
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (fun _ -> dispatch Increment)
+                    Button.content "+"
+                    Button.horizontalAlignment HorizontalAlignment.Stretch
+                ]
+                Button.create [
+                    Button.dock Dock.Bottom
+                    Button.onClick (
+                        (fun _ -> state.count * 2 |> SetCount |> dispatch),
+                        SubPatchOptions.OnChangeOf state.count
+                    )
+                    Button.content "x2"
+                    Button.horizontalAlignment HorizontalAlignment.Stretch
+                ]
+                TextBox.create [
+                    TextBox.dock Dock.Bottom
+                    TextBox.onTextChanged (
+                        (fun text ->
+                            let isNumber, number = System.Int32.TryParse text
+
+                            if isNumber then
+                                number |> SetCount |> dispatch)
+                    )
+                    TextBox.text (string state.count)
+                    TextBox.horizontalAlignment HorizontalAlignment.Stretch
+                ]
+                TextBlock.create [
+                    TextBlock.dock Dock.Top
+                    TextBlock.fontSize 48.0
+                    TextBlock.verticalAlignment VerticalAlignment.Center
+                    TextBlock.horizontalAlignment HorizontalAlignment.Center
+                    TextBlock.text (string state.count)
+                ]
+                VideoView.create [VideoView.mediaPlayer player]
+            ]
+        ]
+
+open Elmish
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI
+open Avalonia.FuncUI.Elmish
+open Avalonia.FuncUI.Components.Hosts
+open Avalonia.Media
+
+type MainWindow() as this =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Counter Example"
+        base.Height <- 400.0
+        base.Width <- 400.0
+
+        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+        Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
+        |> Program.withHost this
+        |> Program.withConsoleTrace
+        |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme(baseUri = null, Mode = FluentThemeMode.Dark))
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main (args: string []) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
