db2798b3c5de571a6e2054dd11c0064c3b7c1425
rururu-orca <87410304+rururu-orca@users.noreply.github.com>
2022-03-26 17:37:31 +0900

    Fix :PlayerLib.FFmpeg.resizeMediaAsync

--- src/RandomSceneDrawing.PlayerLib/FFmpeg.fs
+++ src/RandomSceneDrawing.PlayerLib/FFmpeg.fs
@@ -5,25 +5,31 @@ open Cysharp.Diagnostics
 open FsToolkit.ErrorHandling
 open RandomSceneDrawing.Util
 open FSharp.Control
-open System.Threading.Tasks
+
+module ProcessAsyncEnumerable =
+    open System.Collections.Generic
+
+    let addListAsync (list: List<_>) (output: ProcessAsyncEnumerable) =
+        AsyncSeq.ofAsyncEnum output
+        |> AsyncSeq.iter list.Add
+
 
 let inline runAsync args =
     task {
         let args' = String.concat " " args
         let startInfo = ProcessStartInfo(config.PlayerLib.FFmpegPath, Arguments = args')
+        let errs = System.Collections.Generic.List()
 
         try
-            let struct (_, stdout, stderr) = ProcessX.GetDualAsyncEnumerable(startInfo)
+            let struct (_, out, err) = ProcessX.GetDualAsyncEnumerable(startInfo)
 
-            do!
-                [| stdout.WriteLineAllAsync()
-                   stderr.WriteLineAllAsync() |]
-                |> Task.WhenAll
+            do! ProcessAsyncEnumerable.addListAsync errs err
+            do! out.WriteLineAllAsync()
 
             return Ok()
         with
         | :? ProcessErrorException as ex ->
-            let msg = $"%A{ex.ErrorOutput}\n\n{stderr}\n"
+            let msg = $"ExitCode{ex.ExitCode}: %A{List.ofSeq errs}"
             return Error msg
     }
 
@@ -32,8 +38,24 @@ let inline trimMediaAsync startTime endTime inputPath destinationPath =
     |> runAsync
 
 let inline resizeMediaAsync inputPath rescaleParam destinationPath =
-    [ "-loglevel warning -y -hwaccel cuda -hwaccel_output_format cuda -init_hw_device vulkan=vk:0 -filter_hw_device vk"
-      $"-copyts -i \"{inputPath}\""
-      $"-vf \"hwupload,libplacebo={rescaleParam}:p010le,hwupload_cuda\""
-      $"-c:v hevc_nvenc -an \"{destinationPath}\"" ]
-    |> runAsync
+    let nvenc =
+        [ "-loglevel warning -y -hwaccel cuda -hwaccel_output_format cuda -init_hw_device vulkan=vk:0 -filter_hw_device vk"
+          $"-copyts -i \"{inputPath}\""
+          $"-vf \"hwupload,libplacebo={rescaleParam}:p010le,hwupload_cuda\""
+          $"-c:v hevc_nvenc -an \"{destinationPath}\"" ]
+
+    let qsv =
+        [ "-loglevel warning -y"
+          $"-copyts -i \"{inputPath}\""
+          $"-vf \"scale={rescaleParam}\""
+          $"-c:v hevc_qsv -an \"{destinationPath}\"" ]
+
+    let any =
+        [ "-loglevel warning -y"
+          $"-copyts -i \"{inputPath}\""
+          $"-vf \"scale={rescaleParam}\""
+          $"-c:v hevc -an \"{destinationPath}\"" ]
+
+    runAsync nvenc
+    |> TaskResult.orElse (runAsync qsv)
+    |> TaskResult.orElse (runAsync any)
